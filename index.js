adb3aba856528c7b7257ceb3071208604b129710
William Liang Wang.com <williamlw999@gmail.com>
2020-05-06 10:48:38 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,7 @@
+// dependencies
+const Airtable = require('airtable');
+const Discord = require('discord.js');
+const moment = require('moment-timezone');
+
+// auth file
+const auth = require('./auth.json');
